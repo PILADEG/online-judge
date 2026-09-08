93613156fd1e2a70b034e762c741b8274fc9a082
kun <kun@local>
2026-09-09 01:17:25 +0800

    feat(user): add datasource/mybatis-plus/redisson/jwt config to user-service

---
+++ onlinejudge-user-service/src/main/java/com/kun/onlinejudge/userservice/config/RedissonConfig.java
@@ -0,0 +1,35 @@
+package com.kun.onlinejudge.userservice.config;
+
+import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+@Data
+public class RedissonConfig {
+
+    private String host;
+    private int port;
+    private int database;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + host + ":" + port).setDatabase(database);
+        return Redisson.create(config);
+    }
+
+    @Bean
+    public RedissonClient scriptRedissonClient() {
+        Config config = new Config();
+        config.setCodec(StringCodec.INSTANCE);
+        config.useSingleServer().setAddress("redis://" + host + ":" + port).setDatabase(database);
+        return Redisson.create(config);
+    }
+}
